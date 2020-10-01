40f52df2cb7ca1d63c9e5764e42ea51511379327
Jesse Mader (Kutline) <42048537+IKutline@users.noreply.github.com>
2020-10-01 16:23:04 -0400

    removed unnecessary code from testing

--- remotetest.js
+++ remotetest.js
@@ -1,16 +1,6 @@
 
-
-
-function respond(){
-    message.channel.send("Response");
-}
-
-function respond_SEC(){
-    message.channel.send("Response");
-}
-
 function cmd(m){
-return (m.startsWith('.')) ? respond() : respond_SEC();
+    m.channel.send("Response Primary");
 }
 
 module.exports = {
